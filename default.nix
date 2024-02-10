f70d7dc9956459d36fe02d1d8eda3edfa498e335
Sergio Miguéns <lonyelon@gmail.com>
2024-02-10 23:41:23 +0000

    Update default.nix

--- default.nix
+++ default.nix
@@ -59,13 +59,13 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "orca-slicer";
-  version = "1.7.0";
+  version = "1.9.0";
 
   src = fetchFromGitHub {
     owner = "SoftFever";
     repo = "OrcaSlicer";
     rev = "v${version}";
-    hash = "sha256-qwlLHtTVIRW1g5WRQ5NhDh+O8Bypra/voIu39yU2tbw=";
+    hash = "";
   };
 
   patches = [
